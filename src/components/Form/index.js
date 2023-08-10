8528aecf9367d78be5a66b431a25e8484b4e3936
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-10 16:37:30 -0300

    refatorando o Form componente de class para function component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Warning from "../Warning";
 
-class Form extends React.Component{
+const REGEXP_EMAIL = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+
+/*class Form extends React.Component{
   constructor(props) {
     super(props);
 
@@ -96,3 +98,70 @@ class Form extends React.Component{
 }
 
 export default Form;
+*/
+
+export default function Form() {
+  const [datas, setDatas] = useState({
+    email: "",
+    isValidEmail: false,
+    message: ""
+  });
+
+  function handleSubmitForm(e) {
+    e.preventDefault();
+    
+    if(datas.email === "" || datas.email.trim() === '') {
+      setDatas({...datas, isValidEmail: false, message: "Whoops! It looks like you forgot to add your email"})
+    }
+
+    if(datas.email !== "" && !REGEXP_EMAIL.test(datas.email)) {
+      setDatas({ ...datas, isValidEmail: false, message: "Please provide a valid email address" });
+    }
+
+    if (REGEXP_EMAIL.test(datas.email)) {
+      setDatas({...datas, isValidEmail: true, message: "Good!"});
+    }
+  }
+
+  function handleChangeInput(e) {
+    setDatas({...datas, email: e.target.value});
+  }
+
+  return (
+      <form
+        name="form"
+        className={datas.validEmail ? "form" : "form form_mg"}
+        aria-label="Form subscribe notifications"
+        onSubmit={handleSubmitForm}
+      >
+        <div className="form__group">
+          <div className="form__group-input">
+            <input
+              type="text"
+              name="email"
+              value={datas.email}
+              className={
+                datas.validEmail
+                  ? "input input__email"
+                  : "input input__email input__email_invalid"
+              }
+              placeholder="Your email address..."
+              aria-label="Your email address..."
+              onChange={handleChangeInput}
+              id="email"
+            />
+            {datas.message && <Warning warning={datas.message} />}
+          </div>
+          <button
+            type="submit"
+            className="button button__submit"
+            value="Notify Me"
+            aria-pressed="true"
+            aria-label="Notify Me"
+          >
+            Notify Me
+          </button>
+        </div>
+      </form>
+  );
+}
