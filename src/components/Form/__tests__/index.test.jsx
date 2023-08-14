70c478544f8a07e5e04821e2d3bb8828ba87431b
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-14 14:56:41 -0300

    test: add tests para o componente Form

---
+++ src/components/Form/__tests__/index.test.jsx
@@ -0,0 +1,65 @@
+import "@testing-library/jest-dom";
+import {screen, render, fireEvent} from "@testing-library/react";
+import Form from "..";
+
+describe("Form component", () => {
+    test("deve retornar string vazia input", () => {
+        render(<Form />);
+        const input = screen.getByLabelText("Your email address...");
+        fireEvent.change(input, {
+            target: {
+                value: ""
+            }
+        });
+        expect(input.value).toBe("");
+    });
+
+    test("deve retornar teste@email.com input", () => {
+        render(<Form />);
+        const input = screen.getByLabelText("Your email address...");
+        fireEvent.change(input, {
+            target: {
+                value: "teste@email.com"
+            }
+        });
+        expect(input.value).toBe("teste@email.com");
+    });
+
+    test("não deve mostrar nenhuma mensagem de erro quando o componente é carregado", () => {
+        render(<Form />);
+        const alertElement = screen.queryByRole("alert");
+        expect(alertElement).not.toBeInTheDocument();
+    });
+
+    it("deve mostrar mensagem de erro quando o campo email não for inserido", () => {
+        render(<Form />);
+        const buttonElement = screen.getByRole("button");
+        fireEvent.click(buttonElement);
+        const alertElement = screen.getByRole("alert");
+        expect(alertElement).toBeInTheDocument();
+    });
+
+    it("deve mostrar mensagem de sucesso quando o registro for bem-sucedido.", () => {
+        render(<Form />);
+        const input = screen.getByLabelText("Your email address...");
+        fireEvent.change(input, {
+            target: {
+                value: "teste@email.com"
+            }
+        });
+        const buttonElement = screen.getByRole("button");
+        fireEvent.click(buttonElement);
+        const sucessElement = screen.getByText("Good!");
+        expect(sucessElement).toBeInTheDocument();
+    });
+
+    test("renderiza o componente Form corretamente", () => {
+        render(<Form />);
+        const input = screen.getByLabelText("Your email address...");
+        const button = screen.getByRole("button");
+        expect(input).toBeInTheDocument();
+        expect(button).toBeInTheDocument();
+    });
+});
+
+
